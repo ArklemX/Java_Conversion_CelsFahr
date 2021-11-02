4ce54840db02e5507d8b0e167b6bbc12d2049edb
ArklemX <ArklemX@github.com>
2021-11-02 05:39:42 +0100

    conversion opertionnelle

--- src/App.java
+++ src/App.java
@@ -1,5 +1,43 @@
+import java.util.Scanner;
+
 public class App {
+            //Déclaration des variables utiles
+            static int conversion;
+            static double valeur;
+            static char encore = 'O';
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //Instanciation de l'objet Scanner
+        Scanner scan = new Scanner(System.in);
+
+        // Boucle pour refaire une conversion
+        do {
+            //Début
+                //Boucle secondaire de selection du mode de convesion
+                do {
+                    System.out.println("Quelle Conversion effecué ? Il n'y a que ces 2 choix possibles : \n 1-Celsius vers Fahrenheit\n 2-Fharenheit vers Celsius");
+                    conversion = scan.nextInt();
+                    System.out.println("Quelle valeur convertir ? ");
+                    valeur = scan.nextDouble();
+                    switch (conversion) {
+                        case 1:
+                            System.out.println(valeur + " °C correspond à "+( (   (9.0/5.0) * valeur  ) + 32) +" °F ");
+                            break;
+                        case 2:
+                            System.out.println(valeur+" °F correspond à "+ ( (  (valeur - 32) * 5.0   ) / 9.0 )  +" °C ");
+                            break;
+                        default:
+                        valeur = -1;
+                        System.out.println(" Mode de conversion inconnu !!!\nInconnu !! Mode 1 ou Mode 2 ???\n");
+                            break;
+                    }
+                } while (valeur == -1);
+            //Fin
+
+            System.out.println("Nouvelle Conversion  ? (O/N)");
+            scan.nextLine();
+            encore = scan.nextLine().charAt(0);
+        } while (encore == 'O');
+        //Fermons le flux de l'objet Scanner
+        scan.close();
     }
 }
