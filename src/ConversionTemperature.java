0266e406e50aaf0fbef08a6a7cd171d3ff3915d3
ArklemX <ArklemX@github.com>
2021-11-02 05:41:01 +0100

    Changement nom classe App->Conversion...

--- src/ConversionTemperature.java
+++ src/ConversionTemperature.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class App {
+public class ConversionTemperature {
             //Déclaration des variables utiles
             static int conversion;
             static double valeur;
